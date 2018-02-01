e065811252aa982d3177491dcbfb26279764b307
alkempl <alexblack191198@gmail.com>
2018-02-01 16:49:29 +0300

    Add action to logout btn FINALLY DONE again

--- app/src/main/java/com/pelicanus/insight/ProfileActivity.java
+++ app/src/main/java/com/pelicanus/insight/ProfileActivity.java
@@ -2,12 +2,16 @@ package com.pelicanus.insight;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -45,8 +49,26 @@ public class ProfileActivity extends AppCompatActivity {
         //getting current user
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
-
+        NavigationView navigation = findViewById(R.id.navigation);
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id) {
+                    case R.id.dm_logout:
+                        logOut();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
+
+    public void logOut() {
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(this, LoginActivity.class));
+    }
 
 }
