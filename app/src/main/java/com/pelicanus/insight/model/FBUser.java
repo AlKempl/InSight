e114b5cf1e1723b0b405fddeb33bb4c3762d6e57
AlKempl <alexblack191198@gmail.com>
2018-02-12 18:06:14 +0300

    Dealing with fb
    * Add class model.FBUser
    * Add class service.FirebaseAuthService

---
+++ app/src/main/java/com/pelicanus/insight/model/FBUser.java
@@ -0,0 +1,31 @@
+package com.pelicanus.insight.model;
+
+import android.net.Uri;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import lombok.Data;
+
+@IgnoreExtraProperties
+@Data
+public class FBUser {
+
+    public String id;
+    public String name;
+    public String email;
+    public Boolean verifiedEmail;
+    public Uri photoUri;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public FBUser() {
+    }
+
+    public FBUser(String id, String name, String email, Boolean verifiedEmail, Uri photoUri) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.verifiedEmail = verifiedEmail;
+        this.photoUri = photoUri;
+    }
+}
---
+++ app/src/main/java/com/pelicanus/insight/service/FirebaseAuthService.java
@@ -0,0 +1,50 @@
+package com.pelicanus.insight.service;
+
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.pelicanus.insight.model.FBUser;
+
+
+/**
+ * Created by alkempl on 07.02.18.
+ */
+
+public class FirebaseAuthService {
+
+    /**
+     * Private constructor to prevent instantiation
+     */
+    public FirebaseAuthService() {
+    }
+
+    public FBUser getFBUserData() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            String uid = user.getUid();
+            return new FBUser(uid, name, email, emailVerified, photoUrl);
+        } else {
+            Log.e("ERR", "userInit called without real auth user");
+            return new FBUser("no-id", "No user", "no-email@dot.com", false, null);
+        }
+    }
+
+    public boolean checkFBUserExistenceInDB() {
+
+        return true;
+    }
+
+}
