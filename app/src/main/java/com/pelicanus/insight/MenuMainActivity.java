8682d0a0a2c82dc518e3e1dabb43c08cbdcd56fb
DasIgnis <jana.zadvernyak@gmail.com>
2018-02-27 18:03:41 +0300

    WIP on main menu

--- app/src/main/java/com/pelicanus/insight/MenuMainActivity.java
+++ app/src/main/java/com/pelicanus/insight/MenuMainActivity.java
@@ -1,8 +1,10 @@
 package com.pelicanus.insight;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +15,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.pelicanus.insight.service.SwipeAdapter;
+
 public class MenuMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    ViewPager viewPager;
+    SwipeAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,7 @@ public class MenuMainActivity extends AppCompatActivity
         setContentView(R.layout.activity_menu_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +50,11 @@ public class MenuMainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        viewPager = findViewById(R.id.mainMenuViewPager);
+        adapter = new SwipeAdapter(this);
+        viewPager.setAdapter(adapter);
+
     }
 
     @Override
@@ -80,17 +95,17 @@ public class MenuMainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_myexc) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_create) {
+            startActivity(new Intent(this, CreateTrip.class));
+        } else if (id == R.id.nav_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+        } else if (id == R.id.nav_about) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
---
+++ app/src/main/java/com/pelicanus/insight/service/SwipeAdapter.java
@@ -0,0 +1,63 @@
+package com.pelicanus.insight.service;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.pelicanus.insight.R;
+
+/**
+ * Created by acer on 24.02.2018.
+ */
+
+public class SwipeAdapter extends PagerAdapter {
+
+    private int [] images = {R.drawable.main_menu_excursions_search, R.drawable.main_menu_excursions_add};
+    private java.lang.String [] headers = {"Explore", "Share"};
+    private Context context;
+    private LayoutInflater layoutInflater;
+
+    public SwipeAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == (LinearLayout)object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.menu_main_swipe_element, null);
+
+        ImageView imageView = view.findViewById(R.id.swipe_icon);
+        TextView textView1 = view.findViewById(R.id.swipe_header);
+        TextView textView2 = view.findViewById(R.id.swipe_description);
+
+        imageView.setImageResource(images[position]);
+        textView1.setText(headers[position]);
+        textView2.setText(headers[position]);
+
+        container.addView(view, 0);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
