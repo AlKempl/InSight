9139715dc99366260179d54b1963606af47f68ce
DasIgnis <jana.zadvernyak@gmail.com>
2018-03-05 18:33:14 +0300

    Added icon, links, id, TODOs

--- app/src/main/java/com/pelicanus/insight/MenuMainActivity.java
+++ app/src/main/java/com/pelicanus/insight/MenuMainActivity.java
@@ -14,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MenuMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -26,6 +28,17 @@ public class MenuMainActivity extends AppCompatActivity
         setContentView(R.layout.activity_menu_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Views for filling recently added part
+        //TODO Add function to get names and images of 3 most recent excursions and assign them to views
+        //TODO Add links to 3 most recent excursions
+        ImageView ra_excursions_1_img = findViewById(R.id.ra_excursion_1_img);
+        ImageView ra_excursions_2_img = findViewById(R.id.ra_excursion_2_img);
+        ImageView ra_excursions_3_img = findViewById(R.id.ra_excursion_3_img);
+
+        TextView ra_excursions_1_name = findViewById(R.id.ra_excursion_1_name);
+        TextView ra_excursions_2_name = findViewById(R.id.ra_excursion_2_name);
+        TextView ra_excursions_3_name = findViewById(R.id.ra_excursion_3_name);
 
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -104,4 +117,18 @@ public class MenuMainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    public void OpenCreateTripActivity(View view)
+    {
+        startActivity(new Intent(this, CreateTrip.class));
+    }
+
+    public void OpenTripList(View view)
+    {
+        startActivity(new Intent(this, TripList.class));
+    }
+
 }
+
+
