4179dafae4b9148518cf8a2442b95b49997315e4
DasIgnis <jana.zadvernyak@gmail.com>
2018-03-31 19:35:09 +0300

    Finished design of settings

---
+++ app/src/main/java/com/pelicanus/insight/EditProfileActiity.java
@@ -0,0 +1,13 @@
+package com.pelicanus.insight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditProfileActiity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile_actiity);
+    }
+}
