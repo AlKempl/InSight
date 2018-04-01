5c35aee19cef55ddff2f363607c2cdb1e81c518a
RobolabGs2 <slavik_elk@mail.ru>
2018-04-01 17:53:59 +0300

    Отписка от экскурсии

--- app/src/main/java/com/pelicanus/insight/ExcursionViewActivity.java
+++ app/src/main/java/com/pelicanus/insight/ExcursionViewActivity.java
@@ -13,29 +13,31 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.pelicanus.insight.model.Picture;
-import com.pelicanus.insight.model.User;
-
-import java.util.HashMap;
 
 public class ExcursionViewActivity extends AppCompatActivity {
 
     DatabaseReference reference;
     String author_id;
-    String username;
+    Button multi_btn;
+    String userid;
+    enum ButtonMode{Im_in,Im_out,Edit}
+    ButtonMode buttonMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_excursion_view);
         reference = FirebaseDatabase.getInstance().getReference();
+
+        multi_btn =findViewById(R.id.im_in_btn);
+
+        userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         TextView ex_name = findViewById(R.id.view_excursion_name);
         TextView ex_description = findViewById(R.id.view_description);
@@ -59,25 +61,91 @@ public class ExcursionViewActivity extends AppCompatActivity {
         new Picture((ImageView) findViewById(R.id.view_author_image), Picture.Type.User_avatar, author_id).Download();
         new Picture((ImageView) findViewById(R.id.view_trip_image), Picture.Type.Trip_avatar, trip_id).Download();
 
+        buttonMode=ButtonMode.Im_in;
 
-        Button im_in=findViewById(R.id.im_in_btn);
 
-        final String userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        if(userid==author_id) {
+            multi_btn.setText("Edit");
+            buttonMode=ButtonMode.Edit;
 
-        im_in.setOnClickListener(new View.OnClickListener() {
+        }
+        else{
+            reference.child("Visitors").child(trip_id).addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    if(dataSnapshot.hasChild(userid)) {
+                        multi_btn.setText("I'm out");
+                        buttonMode = ButtonMode.Im_out;
+                    }
+                    reference.child("Visitors").child(trip_id).removeEventListener(this);
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+        multi_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                reference.child("Visitors").child(trip_id).child(userid).setValue(false).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful())
-                            Toast.makeText(getApplicationContext(),"Вы записаны на экскурсию",Toast.LENGTH_LONG).show();
-                        else
-                            Toast.makeText(getApplicationContext(),"FAIL",Toast.LENGTH_LONG).show();
+                switch (buttonMode){
+                    case Im_in:{
+                        if(buttonMode==ButtonMode.Im_in){
+                            reference.child("Visitors").child(trip_id).child(userid).setValue(false).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                        Toast.makeText(getApplicationContext(),"Вы записаны на экскурсию",Toast.LENGTH_LONG).show();
+                                    else
+                                        Toast.makeText(getApplicationContext(),"FAIL",Toast.LENGTH_LONG).show();
+                                }
+                            });
+                            buttonMode=ButtonMode.Im_out;
+                            multi_btn.setText("I'm out");
                     }
-                });
-            }
+
+                    }
+                    break;
+                    case Im_out:{
+                        reference.child("Visitors").child(trip_id).child(userid).setValue(null).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful())
+                                    Toast.makeText(getApplicationContext(),"Вы отписаны на экскурсию",Toast.LENGTH_LONG).show();
+                                else
+                                    Toast.makeText(getApplicationContext(),"FAIL",Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        multi_btn.setText("I'm in");
+                        buttonMode=ButtonMode.Im_in;
+                    }
+                    break;
+                    case Edit:{
+                        //TODO Переход на активити редактирования экскурсии
+                    }
+                    break;
+
+
+            }}
         });
 
 
